3c4d18d065eb81e6e39df5731fac6e33d2336ae6
Sean McPherson <git@seanmcp.com>
2020-05-15 09:35:20 -0400

    Create basic-ticktick.user.js

---
+++ src/basic-ticktick.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Basic TickTick
+// @namespace    https://seanmcp.com
+// @version      0.1
+// @description  CSS to remove all premium features/prompts from TickTick
+// @author       Sean McPherson
+// @match        https://ticktick.com/webapp/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    const style = document.createElement('style')
+    style.id = "__basicTickTick"
+
+    style.textContent = `
+#left-bottom-view,
+.sub-task a.reminder-hint,
+.tkcalendar,
+[projectid="summary"],
+a[href$="tasks/activity"],
+a.l-tab-csl {
+    display: none !important;
+}
+`
+
+    document.head.appendChild(style)
+})();
